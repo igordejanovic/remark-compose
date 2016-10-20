923ff7aad180c154cef190c7a996aaaff1418424
Igor Dejanovic <igor.dejanovic@gmail.com>
2016-10-20 11:02:36 +0200

    Render input trough template engine. Added 'now' datetime object. Relative rconf paths.

--- remarkcompose/cli.py
+++ remarkcompose/cli.py
@@ -4,6 +4,7 @@ import click
 import livereload
 import jinja2
 import glob2
+from datetime import datetime
 from .rconf import get_rconf_meta
 from .exceptions import RComposeException
 
@@ -23,16 +24,152 @@ remarkc = RComposeCLI(
     help='Remark slides building from template with live HTTP server.')
 
 
-def _find_files(glob_pattern):
+jinja_env = None
+
+
+@click.command()
+@click.argument('rconf_file')
+@click.option('-p', '--port', default=9090,
+              help='Port to listen to. Default is 9090.')
+def serve(rconf_file, port):
+    """
+    Watch input markdown files for change and regenerates target files.
+    """
+    if not rconf_file.endswith('.rconf'):
+        rconf_file += '.rconf'
+
+    def _get_from_rel(f):
+        """Returns path given relatively to rconf file."""
+        return os.path.join(os.path.dirname(rconf_file), f)
+
+    try:
+        rconf_model = _load_rconf(rconf_file)
+
+        def do_build():
+            _internal_build(rconf_file)
+
+        watch_files = set()
+
+        template_file = _get_param(rconf_model, "template")
+        if template_file:
+            watch_files.add(_get_from_rel(template_file))
+
+        for rule in rconf_model.rules:
+            watch_files.update(_find_files(rule.input_file,
+                                           parent=rconf_file,
+                                           strip_parent=False))
+
+            # Add template file if the rule overrides global template
+            watch_files.add(_get_from_rel(_get_param(rconf_model, "template")))
+
+        server = livereload.Server()
+        for f in watch_files:
+            server.watch(f, do_build)
+
+        server.serve(port=port, root=os.path.dirname(rconf_file))
+
+    except RComposeException as e:
+        click.echo(e)
+
+
+@click.command()
+@click.argument('rconf_file')
+def build(rconf_file):
+    """
+    Generates target html files from markdown files and HTML template.
+    """
+
+    try:
+        _internal_build(rconf_file)
+    except RComposeException as e:
+        click.echo(e)
+
+
+def _internal_build(rconf_file):
+
+    global jinja_env
+
+    if not rconf_file.endswith('.rconf'):
+        rconf_file += '.rconf'
+
+    # Loading of templates and input files is relative to rconf model
+    jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(os.path.dirname(rconf_file)))
+
+    jinja_env.filters['dtformat'] = _dtformat
+
+    rconf_model = _load_rconf(rconf_file)
+
+    click.echo("Building output files...")
+
+    def _gen_html(input_file, main_template, params, output_file):
+        global jinja_env
+
+        # Content input file is processed by template engine
+        params['now'] = datetime.now()
+        content = jinja_env.get_template(input_file).render(**params)
+
+        # Pass rendered input as the parameter to output html template
+        params['content'] = content
+
+        # Calculate output file name
+
+        base_name = os.path.basename(input_file)
+        base_name = os.path.splitext(base_name)[0]
+
+        if not output_file:
+            output_file = os.path.join(
+                os.path.dirname(os.path.join(os.path.dirname(rconf_file),
+                                             input_file)),
+                "{}.html".format(base_name))
+        elif os.path.isdir(output_file):
+            output_file = os.path.join(output_file,
+                                       "{}.html".format(base_name))
+
+        click.echo(output_file)
+
+        t = jinja_env.get_template(main_template)
+        with open(output_file, 'w') as f:
+            f.write(t.render(**params))
+
+    global_params = {p.name: p.value for p in rconf_model.params}
+
+    for rule in rconf_model.rules:
+        rule_params = {p.name: p.value for p in rule.params}
+
+        # Take global params and override with rule params
+        params = dict(global_params)
+        params.update(rule_params)
+
+        for f in _find_files(rule.input_file, parent=rconf_file):
+            _gen_html(f, _get_param(rule, "template"), params, rule.output_file)
+
+
+def _find_files(glob_pattern, parent=None, strip_parent=True):
+    """Searces for files given by glob_pattern relative to parent."""
+
     input_files = []
+
+    if parent:
+        parent = os.path.dirname(parent)
+    if parent:
+        glob_pattern = os.path.join(parent, glob_pattern)
+
     for f in glob2.glob(glob_pattern):
         input_files.append(f)
+
+    # Strip parent from founded names
+    if parent and strip_parent:
+        input_files = [os.path.relpath(x, parent) for x in input_files]
+
     return input_files
 
 
+def _join_dir(parent, f):
+    return os.path.join(os.path.dirname(parent), f)
+
+
 def _load_rconf(rconf_file):
-    if not rconf_file.endswith('.rconf'):
-        rconf_file += '.rconf'
     return get_rconf_meta().model_from_file(rconf_file)
 
 
@@ -52,92 +189,5 @@ def _get_param(obj, name):
     raise RComposeException('"{}" parameter is not defined.'.format(name))
 
 
-@click.command()
-@click.argument('rconf_file')
-@click.option('-p', '--port', default=9090,
-              help='Port to listen to. Default is 9090.')
-def serve(rconf_file, port):
-    """
-    Watch input markdown files for change and regenerates target files.
-    """
-    try:
-        rconf_model = _load_rconf(rconf_file)
-
-        def do_build():
-            _internal_build(rconf_file)
-
-        watch_files = set()
-
-        template_file = _get_param(rconf_model, "template")
-        if template_file:
-            watch_files.add(template_file)
-
-        for rule in rconf_model.rules:
-            watch_files.update(_find_files(rule.input_file))
-
-            # Add template file if the rule overrides global template
-            watch_files.add(_get_param(rconf_model, "template"))
-
-        server = livereload.Server()
-        for f in watch_files:
-            server.watch(f, do_build)
-
-        server.serve(port=port)
-
-    except RComposeException as e:
-        click.echo(e)
-
-
-@click.command()
-@click.argument('rconf_file')
-def build(rconf_file):
-    """
-    Generates target html files from markdown files and HTML template.
-    """
-    try:
-        _internal_build(rconf_file)
-    except RComposeException as e:
-        click.echo(e)
-
-
-def _internal_build(rconf_file):
-
-    rconf_model = _load_rconf(rconf_file)
-
-    click.echo("Building output files...")
-
-    def _gen_html(input_file, template, params, output_file):
-
-        with open(input_file, 'r') as f:
-            content = f.read()
-            params['content'] = content
-
-        base_name = os.path.basename(input_file)
-        base_name = os.path.splitext(base_name)[0]
-
-        if not output_file:
-            output_file = os.path.join(os.path.dirname(input_file),
-                                       "{}.html".format(base_name))
-        elif os.path.isdir(output_file):
-            output_file = os.path.join(output_file,
-                                       "{}.html".format(base_name))
-
-        click.echo(output_file)
-
-        with open(output_file, 'w') as f:
-            f.write(template.render(**params))
-
-    global_params = {p.name: p.value for p in rconf_model.params}
-
-    for rule in rconf_model.rules:
-        rule_params = {p.name: p.value for p in rule.params}
-
-        # Take global params and override with rule params
-        params = dict(global_params)
-        params.update(rule_params)
-
-        with open(_get_param(rule, "template"), 'r') as f:
-            t = jinja2.Template(f.read())
-
-        for f in _find_files(rule.input_file):
-            _gen_html(f, t, params, rule.output_file)
+def _dtformat(value, format='%Y-%m-%d %H:%M'):
+    return value.strftime(format)
